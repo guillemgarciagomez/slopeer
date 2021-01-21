9acf828709e4d75bc43985d482fc7d10d9403191
Pierre <pierrelejay1@gmail.com>
2021-01-21 12:02:36 +0000

    content: 'Installed nodemon and commitizen, added Mapbox tokens to the front end'
    'Installed nodemon and commitizen, added Mapbox tokens to the front end'
    
    N/A

--- client/config.example.js
+++ client/config.example.js
@@ -1,7 +1,7 @@
 module.exports = {
-  mapboxToken: '',
+  mapboxToken: 'pk.eyJ1IjoiZ3NqdXJhZG9nb24iLCJhIjoiY2trNm9jZmdrMDJ3bTJubG44YjVydTJyayJ9.GdTYmk-tuocBHtD9ZAe-Dg',
   mapboxStyle: '',
-  serverUrl: '',
+  serverUrl: 'http//localhost:4000',
   profilePicturesUrl: 'https://slopeer-public.s3.eu-central-1.amazonaws.com/profile_pictures/',
   routePicturesUrl: 'https://slopeer-public.s3.eu-central-1.amazonaws.com/route_pictures/'
 };
