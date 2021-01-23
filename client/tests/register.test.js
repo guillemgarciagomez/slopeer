b90b4ef55136cea65c085816b3d3a8cfd8f9633e
gsjuradog <gsjuradog@unal.edu.co>
2021-01-23 18:31:17 +0100

    New files for tests setup

---
+++ client/tests/register.test.js
@@ -0,0 +1,26 @@
+import { h } from 'preact';
+import Register from '../../src/routes/register';
+
+import { render, fireEvent, screen, cleanup } from '@testing-library/preact';
+
+const { debug } = render(<Register />)
+debug()
+test('should be able to register', async ()=> {
+  const {getByText, debug} = render( <Register /> );
+  const text = getByText("SLOPEER" )
+  expect(text).toBeInTheDocument();
+  
+  // const submitButton = getByTestId('submit-button', {name: 'submit'})
+  // // const usernameInput = getByPlaceholderText('Username'); 
+  // // const passwordInput = getByPlaceholderText('Password'); 
+  // // const emailInput = getByPlaceholderText('Email'); 
+
+
+  // fireEvent.click(submitButton).should();
+  
+  debug()
+  // cleanup()
+})
+
+
+
