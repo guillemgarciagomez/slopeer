ca989ab9d4a7617e9b3ebba8770732b442a40bb7
gsjuradog <gsjuradog@unal.edu.co>
2021-01-25 10:33:42 +0100

    Tests setUp modifications

--- client/tests/register.test.js
+++ client/tests/register.test.js
@@ -1,25 +1,31 @@
 import { h } from 'preact';
-import Register from '../src/routes/register';
+import Register from '../src/routes/register/index';
+
 
 import { render, fireEvent, screen, cleanup } from '@testing-library/preact';
-
-const { debug } = render(<Register />)
-debug()
-test('should be able to register', async ()=> {
-  const {getByText, debug} = render( <Register /> );
-  const text = getByText("SLOPEER" )
-  expect(text).toBeInTheDocument();
+describe('Register', ()=> {
+  window.URL.createObjectURL = jest.fn();
+  // const { debug } = render(<Register />)
+  // debug()
+  test('should be able to register', async ()=> {
+   
+    const {debug} = render( <Register /> );
   
-  // const submitButton = getByTestId('submit-button', {name: 'submit'})
-  // // const usernameInput = getByPlaceholderText('Username'); 
-  // // const passwordInput = getByPlaceholderText('Password'); 
-  // // const emailInput = getByPlaceholderText('Email'); 
-
-
-  // fireEvent.click(submitButton).should();
+    const text = screen.getByText("SLOPEER" )
+    window.URL.createObjectURL = jest.fn();
+    expect(text).toBeTruthy();
+    
+    // const submitButton = getByTestId('submit-button', {name: 'submit'})
+    // // const usernameInput = getByPlaceholderText('Username'); 
+    // // const passwordInput = getByPlaceholderText('Password'); 
+    // // const emailInput = getByPlaceholderText('Email'); 
   
-  debug()
-  // cleanup()
+  
+    // fireEvent.click(submitButton).should();
+    
+    debug()
+    // cleanup()
+  })
 })
 
 
