243970585c62ed2a9396f681179a006e0d8090e6
gsjuradog <gsjuradog@unal.edu.co>
2021-01-25 17:05:51 +0100

    Added files for test setUp and changes in package

---
+++ server/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFiles: ['./tests/jest.setup-file.ts'],
+};
---
+++ server/tests/jest.setup-file.ts
@@ -0,0 +1,1 @@
+// set up libraries and imports that you need for your tests to run successfully
