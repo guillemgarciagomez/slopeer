ae58fbc73b749d3d547d098aaece40d5ffd4dac1
gsjuradog <gsjuradog@unal.edu.co>
2021-01-25 21:45:28 +0100

    Tests progress

--- server/tests/basic.test.ts
+++ server/tests/basic.test.ts
@@ -1,21 +1,58 @@
-import {Request} from 'supertest';
+import { ApolloServer, gql } from 'apollo-server-express';
+import {Request, Response} from 'express';
+// import {Request} from 'supertest';
 import mongoose from 'mongoose';
-import connection from '../src/index';
+import { ObjectId } from 'mongodb';
 import { User } from '../src/models';
-import { textSpanIsEmpty } from 'typescript';
-import Resolvers from '../src/graphql/resolvers/index'
+import resolvers from '../src/graphql/resolvers'
 import models from '../src/models/index'
+import typeDefs from '../src/graphql/schemas'
 
 
 import {createTestClient} from 'apollo-server-testing';
 import { Iuser } from '../src/models/user.model';
-const { query, mutate } = createTestClient(server)
+
 const { DB_HOST } = process.env;
+
 const mongoDB:string =  `${DB_HOST}/testDatabase`
-mongoose.connect(mongoDB, { useNewUrlParser: true });
 
+mongoose.connect(mongoDB, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+})
+.catch(error => console.error(error)); 
 
+// const dropTestDb = async () => {
+//   await mongoose.connection.db.dropDatabase().catch(error => console.error(error));
+// }
+
+// const closeDbConnection = async () => {
+//   await mongoose.connection.close().catch(error => console.error(error));;
+// }
+type express= {res:Response, req: Request}  
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: ({
+    context: ({ req, res }:express) => ({ req, res }),
+    uploads: false
+  })
+});
+
+const { query, mutate } = createTestClient(server as any)
 //testing test
+const mockUser:Iuser = {
+  email: 'test7@Empty.com',
+username: 'test7',
+password: 'test77',
+profile_picture: '',
+owned_routes: [],
+saved_routes: []
+}
+
+const routeToBeAdded = {//mock route 
+}
 describe('User model test', ()=>{
   //clear out database 
   beforeAll(async () => {
@@ -37,14 +74,7 @@ describe('User model test', ()=>{
   
   describe('get user', ()=>{
     it('gets a user', async ()=> {
-      const user:Iuser = {
-        email: 'test7@Empty.com',
-  username: 'test7',
-  password: 'test77',
-  profile_picture: '',
-  owned_routes: [],
-  saved_routes: []
-      }
+      
     }) 
   })
 
